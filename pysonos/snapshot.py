d8329a73a8c7983fb81f3193fc771b7deff721c0
Anders Melchiorsen <amelchio@nogoto.net>
2019-03-03 16:39:23 +0100

    Refactor into _restore_volume

--- pysonos/snapshot.py
+++ pysonos/snapshot.py
@@ -172,34 +172,7 @@ class Snapshot(object):
         if self.is_coordinator:
             self._restore_coordinator()
 
-        # For all devices:
-        self.device.mute = self.mute
-
-        # Reinstate volume
-        # Can only change volume on device with fixed volume set to False
-        # otherwise get uPnP error, so check first. Before issuing a network
-        # command to check, fixed volume always has volume set to 100.
-        # So only checked fixed volume if volume is 100.
-        if self.volume == 100:
-            fixed_vol = self.device.renderingControl.GetOutputFixed(
-                [('InstanceID', 0)])['CurrentFixed']
-        else:
-            fixed_vol = False
-
-        # now set volume if not fixed
-        if not fixed_vol:
-            self.device.bass = self.bass
-            self.device.treble = self.treble
-            self.device.loudness = self.loudness
-
-            if fade:
-                # if fade requested in restore
-                # set volume to 0 then fade up to saved volume (non blocking)
-                self.device.volume = 0
-                self.device.ramp_to_volume(self.volume)
-            else:
-                # set volume
-                self.device.volume = self.volume
+        self._restore_volume(fade)
 
         # Now everything is set, see if we need to be playing, stopped
         # or paused ( only for coordinators)
@@ -255,6 +228,39 @@ class Snapshot(object):
             if self.media_uri != "":
                 self.device.play_uri(
                     self.media_uri, self.media_metadata, start=False)
+
+    def _restore_volume(self, fade):
+        """Reinstate volume.
+
+        Args:
+            fade (bool): Whether volume should be faded up on restore.
+        """
+        self.device.mute = self.mute
+
+        # Can only change volume on device with fixed volume set to False
+        # otherwise get uPnP error, so check first. Before issuing a network
+        # command to check, fixed volume always has volume set to 100.
+        # So only checked fixed volume if volume is 100.
+        if self.volume == 100:
+            fixed_vol = self.device.renderingControl.GetOutputFixed(
+                [('InstanceID', 0)])['CurrentFixed']
+        else:
+            fixed_vol = False
+
+        # now set volume if not fixed
+        if not fixed_vol:
+            self.device.bass = self.bass
+            self.device.treble = self.treble
+            self.device.loudness = self.loudness
+
+            if fade:
+                # if fade requested in restore
+                # set volume to 0 then fade up to saved volume (non blocking)
+                self.device.volume = 0
+                self.device.ramp_to_volume(self.volume)
+            else:
+                # set volume
+                self.device.volume = self.volume
 
     def _save_queue(self):
         """Save the current state of the queue."""
