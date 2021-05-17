84dd0d62d619630fafa189338f23754fdf305dd3
Anders Melchiorsen <amelchio@nogoto.net>
2021-05-17 21:53:47 +0200

    Version 0.0.48

--- pysonos/__init__.py
+++ pysonos/__init__.py
@@ -17,7 +17,7 @@ from .exceptions import SoCoException, UnknownSoCoException
 __author__ = "Anders Melchiorsen <amelchio@nogoto.net>"
 # Please increment the version number and add the suffix "-dev" after
 # a release, to make it possible to identify in-development code
-__version__ = "0.0.47"
+__version__ = "0.0.48"
 __website__ = "https://github.com/amelchio/pysonos"
 __license__ = "MIT License"
 
